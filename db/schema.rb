0c2eee576afad1b18a22bf3eedb4d9a4d2108e4f
Tim Jedrek <tim@timjedrek.com>
2025-04-04 02:08:33 -0600

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_04_075242) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_04_080627) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "airports", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.string "city"
+    t.string "nearby_towns"
+    t.bigint "state_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["code"], name: "index_airports_on_code", unique: true
+    t.index ["state_id", "city"], name: "index_airports_on_state_id_and_city"
+    t.index ["state_id"], name: "index_airports_on_state_id"
+  end
 
   create_table "states", force: :cascade do |t|
     t.string "name"
@@ -39,4 +52,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_04_075242) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "airports", "states"
 end
