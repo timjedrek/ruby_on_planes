91a7f5dccd5e9432098338b1b96565cf76655f9a
Tim Jedrek <tim@timjedrek.com>
2025-04-04 02:01:24 -0600

    get the states going

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,7 +1,10 @@
 class StatesController < ApplicationController
   def index
+    @states = State.order(:name)
   end
 
   def show
+    @state = State.find_by!(abbreviation: params[:abbreviation])
+    @airports = @state.airports.order(:city) if @state.airports.any? # Placeholder for now
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+states = [
+  { name: "Missouri", abbreviation: "MO" },
+  { name: "Florida", abbreviation: "FL" },
+  { name: "California", abbreviation: "CA" }
+]
+states.each { |s| State.create!(s) }
