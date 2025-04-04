37a3f0f336b41b1c76c6f4db4853088fc4fdf6dd
Tim Jedrek <tim@timjedrek.com>
2025-04-04 01:53:56 -0600

    edit migration

--- db/migrate/20250404075242_create_states.rb
+++ db/migrate/20250404075242_create_states.rb
@@ -3,8 +3,8 @@ class CreateStates < ActiveRecord::Migration[8.0]
     create_table :states do |t|
       t.string :name
       t.string :abbreviation
-
       t.timestamps
     end
+    add_index :states, :abbreviation, unique: true
   end
 end
