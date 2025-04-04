4c8064edadde768df599d944c9d85b4791d789b2
Tim Jedrek <tim@timjedrek.com>
2025-04-04 01:54:19 -0600

    edit model

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,2 +1,6 @@
 class State < ApplicationRecord
+  has_many :airports, dependent: :restrict_with_error
+
+  validates :name, :abbreviation, presence: true
+  validates :abbreviation, uniqueness: true, length: { is: 2 }
 end
