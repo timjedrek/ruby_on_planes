549d711e5be73f03ac3fb926fe0ec9608959c3af
Tim Jedrek <tim@timjedrek.com>
2025-04-04 00:50:07 -0600

    still working on email confirms

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -5,9 +5,15 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
 
     if resource.errors.empty?
       set_flash_message!(:notice, :confirmed)
-      respond_with_navigational(resource) { redirect_to after_confirmation_path_for(resource_name, resource) }
+      respond_to do |format|
+        format.html { redirect_to after_confirmation_path_for(resource_name, resource) }
+        format.turbo_stream { redirect_to after_confirmation_path_for(resource_name, resource) }
+      end
     else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity) { render :new }
+      respond_to do |format|
+        format.html { render :new, status: :unprocessable_entity }
+        format.turbo_stream { render :new, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -17,7 +23,7 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
     if signed_in?(resource_name)
       root_path
     else
-      new_session_path(resource_name)  # Sign-in page
+      new_session_path(resource_name)  # Redirect to sign-in
     end
   end
 end
