9ba446100c752f47ebbf072695dc040d13d1a54e
Tim Jedrek <tim@timjedrek.com>
2025-04-04 02:08:46 -0600

    seed test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
+# db/seeds.rb
 states = [
   { name: "Missouri", abbreviation: "MO" },
   { name: "Florida", abbreviation: "FL" },
   { name: "California", abbreviation: "CA" }
 ]
 states.each { |s| State.create!(s) }
+
+mo = State.find_by(abbreviation: "MO")
+fl = State.find_by(abbreviation: "FL")
+
+Airport.create!([
+  { code: "STL", name: "St. Louis Lambert International Airport", city: "St. Louis", nearby_towns: ["Chesterfield", "Ballwin"], state: mo },
+  { code: "MCO", name: "Orlando International Airport", city: "Orlando", nearby_towns: ["Winter Park"], state: fl }
+])
