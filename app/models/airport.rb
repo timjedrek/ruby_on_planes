596e097d9630936046c81514c6e6350442a34e84
Tim Jedrek <tim@timjedrek.com>
2025-04-04 02:08:10 -0600

    add validation

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,3 +1,7 @@
 class Airport < ApplicationRecord
   belongs_to :state
+  has_many :schools, dependent: :restrict_with_error
+
+  validates :code, :name, :city, presence: true
+  validates :code, uniqueness: true
 end
