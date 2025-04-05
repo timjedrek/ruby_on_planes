c943827c4a393cddf36a8e6fe57af7adf1d90d23
Tim Jedrek <tim@timjedrek.com>
2025-04-04 23:29:21 -0500

    cities

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,11 +3,9 @@ class CitiesController < ApplicationController
     @state = State.find_by!(abbreviation: params[:state_abbreviation].upcase)
     @city = @state.cities.find_by!(name: params[:name])
     @airports = @city.airports.order(:name)
-    # Nearby airports: from cities that share any of this city's airports
-    @nearby_airports = Airport.joins(:cities)
-                              .where(cities: { state_id: @state.id })
-                              .where.not(id: @airports.pluck(:id)) # Exclude city's own airports
-                              .where(cities: { id: @city.airports.flat_map(&:cities).pluck(:id) })
+    # Nearby airports: from nearby cities in the same state
+    @nearby_airports = Airport.where(city_id: @city.nearby_city_records.pluck(:id), state_id: @state.id)
+                              .where.not(id: @airports.pluck(:id))
                               .order(:name)
                               .distinct
     set_meta_tags title: "Flight Schools in #{@city.name}, #{@state.abbreviation} | Pilot Training Near Me",
